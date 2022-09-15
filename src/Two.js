5af691c01ecba912598473b1de2b59469550628a
AbdelrahmanHafez <Dahomy999@outlook.sa>
2022-09-15 13:20:14 +0200

    install tailwind and bootstrap

---
+++ src/Two.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Two.module.css";
+
+function Two() {
+  return (
+    <div>
+      <h1>page2</h1>
+      <div>
+        <button>hello2</button>
+      </div>
+    </div>
+  );
+}
+
+export default Two;
