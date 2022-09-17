7989105ab3e3d1cd64f839e2ce56e90e7a886a0e
AbdelrahmanHafez <Dahomy999@outlook.sa>
2022-09-17 17:14:12 +0200

    hmbbutton working

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { FaBeer, FaBars } from "react-icons/fa";
 // import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,16 +7,29 @@ import classNames from "classnames";
 import Carousel from "Components/Carousel";
 
 function Home() {
+  const [hmbShowNav, sethmbShowNav] = useState(false);
   return (
     <div className="h-full w-full bg-[url('../public/edugate-bg.JPG')] bg-cover">
       <nav className={styles.nav}>
-        <img
-          src="/Edugate-logo.png"
-          alt="Edugate Logo"
-          className={styles.logo}
-        />
+        <div className="flex space-around">
+          <img
+            src="/Edugate-logo.png"
+            alt="Edugate Logo"
+            className={styles.logo}
+          />
 
-        <ul className={styles.navbuttons}>
+          <div
+            className={styles.hmbmenu}
+            onClick={() => {
+              document.getElementById("navlinks").classList.toggle("flex");
+              document.getElementById("navlinks").classList.toggle("hidden");
+            }}
+          >
+            <FaBars />
+          </div>
+        </div>
+
+        <ul id="navlinks" className={styles.navbuttons}>
           <li className={styles.navbtnActive}>Home</li>
           <li className={styles.navbtn}>Universities in Egypt</li>
           <li className={styles.navbtn}>Traning</li>
@@ -32,10 +45,6 @@ function Home() {
           <a href="/#signup" className={styles.signup}>
             signup
           </a>
-        </div>
-
-        <div className={styles.hmbmenu}>
-          <FaBars />
         </div>
       </nav>
 
@@ -74,20 +83,24 @@ const styles = {
   mccontent: "bg-cyan-500 md:w-8/12 flex flex-col justify-center items-center",
 
   searchBox: "bg-slate-400 w-full md:w-3/4  h-20 mx-auto mb-4",
-  nav: "flex flex-row bg-white bg-opacity-90 fixed top-0 w-full",
+
+  nav: "flex flex-col lg:flex-row bg-white bg-opacity-90 fixed top-0 w-full",
   logo: "ml-5 w-[250px] h-auto m-5",
+  hmbmenu: "lg:hidden flex justify-center items-center ml-auto mr-10 text-3xl",
+
   loginSignupContainer: "hidden mr-5 lg:flex flex-row mb-5",
   login:
     "no-underline text-black px-3 py-2 hover:text-slate-700 flex flex-col-reverse",
   signup:
     "text-white bg-gray-900 p-2 cursor-pointer rounded-b-2xl flex flex-col-reverse ",
-  navbuttons: "hidden lg:flex mx-auto space-x-1 mb-5",
+  navbuttons:
+    "hidden lg:flex flex-col lg:flex-row lg:mx-auto lg:space-x-1 mb-5",
   navbtn:
     "text-black p-2 pointer-events-auto cursor-pointer hover:bg-gray-400 rounded-b-2xl flex flex-col-reverse text-center",
   navbtnActive:
     "text-white bg-gray-900 p-2 cursor-pointer rounded-b-2xl flex flex-col-reverse text-center",
+
   carousel: "w-full h-60 bg-green-600",
-  hmbmenu: "lg:hidden flex justify-center items-center ml-auto mr-10 text-3xl",
 };
 
 // https://stackoverflow.com/questions/66614875/how-can-i-enable-tailwind-intellisense-outside-of-classname
