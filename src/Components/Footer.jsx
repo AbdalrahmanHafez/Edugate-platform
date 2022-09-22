faceaad950ee656e1f7b1a86aa26205c7d13ba12
AbdelrahmanHafez <Dahomy999@outlook.sa>
2022-09-22 20:28:13 +0200

    Footer added Site Map and replaced Icons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { MdLocationOn, MdEmail, MdMail } from "react-icons/md";
 import {
-  AiTwotonePhone,
-  AiFillMobile,
   AiFillFacebook,
   AiFillLinkedin,
   AiFillInstagram,
 } from "react-icons/ai";
+import { GoLocation } from "react-icons/go";
+import { BsTelephone, BsPhone } from "react-icons/bs";
+import { HiOutlineMail } from "react-icons/hi";
 
 const TextWithIcon = ({ icon, text }) => {
   return (
@@ -21,7 +21,7 @@ export default function Footer() {
   return (
     // <footer className="mt-10 p-4 drop-shadow-[0_-5px_5px_rgba(0,0,0,0.20)] rounded-t-lg md:px-6 md:py-8 bg-black">
     <footer className="mt-10 p-4 rounded-t-lg shadow md:px-6 md:py-8 bg-gradient-to-t from-black to-[#291313] ">
-      <div className="flex sm:flex-row flex-col items-start justify-between">
+      <div className="flex sm:flex-row flex-col items-start justify-around">
         <a href="/" className="flex mb-5 sm:mb-0">
           <img
             src="Edugate-new-logo.png"
@@ -29,23 +29,50 @@ export default function Footer() {
             alt="Edugate Logo"
           />
         </a>
+
+        <div className="max-w-[20rem] lg:mb-0 mb-10 flex flex-col gap-y-2 items-start text-sm text-gray-400">
+          <h1 className="font-bold text-lg mb-2">Site Map</h1>
+
+          <a href="/replacemelink" className="ml-5 hover:text-slate-200">
+            Home
+          </a>
+          <a href="/replacemelink" className="ml-5 hover:text-slate-200">
+            Universities in Egypt
+          </a>
+          <a href="/replacemelink" className="ml-5 hover:text-slate-200">
+            Training
+          </a>
+          <a href="/replacemelink" className="ml-5 hover:text-slate-200">
+            Scholarships
+          </a>
+          <a href="/replacemelink" className="ml-5 hover:text-slate-200">
+            Study Abroad
+          </a>
+          <a href="/replacemelink" className="ml-5 hover:text-slate-200">
+            Consulting
+          </a>
+          <a href="/replacemelink" className="ml-5 hover:text-slate-200">
+            Score Calculator
+          </a>
+        </div>
+
         <div className="max-w-[20rem] flex flex-col gap-y-2 items-start text-sm text-gray-400">
-          <h1 className="font-bold text-lg mb-3">Contact Us</h1>
+          <h1 className="font-bold text-lg mb-2">Contact Us</h1>
 
           <TextWithIcon
-            icon={<MdLocationOn />}
+            icon={<GoLocation />}
             text="22 Forsan towers -Laserina Tower - Maadi Ring road - Qatamya - Cairo - Egypt"
           />
 
-          <TextWithIcon icon={<AiTwotonePhone />} text="02 223151026" />
+          <TextWithIcon icon={<BsTelephone />} text="02 223151026" />
 
           <TextWithIcon
-            icon={<AiFillMobile />}
+            icon={<BsPhone />}
             text="+201000429759 / +201012690901"
           />
 
           <TextWithIcon
-            icon={<MdMail />}
+            icon={<HiOutlineMail />}
             text={
               <a
                 href="mailto:info@edugate-eg.com"
