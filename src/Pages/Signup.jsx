8df71f930c35c72d2c415bc63afcc60f904dc9a9
AbdelrahmanHafez <Dahomy999@outlook.sa>
2022-10-06 13:18:29 +0200

    Signup Step 2,3

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -9,12 +9,19 @@ import Button from "@mui/material/Button";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
 import Checkbox from "@mui/material/Checkbox";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import {
   createTheme,
   Styled,
   Theme,
   ThemeProvider,
 } from "@mui/material/styles";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormLabel from "@mui/material/FormLabel";
 
 const CustomTextField = ({ ...params }) => (
   <TextField
@@ -63,144 +70,274 @@ const STEP_COUNT = 3;
 
 function SignupForm({ activeStep }) {
   return [
-    <ThemeProvider theme={theme}>
-      <div className="flex flex-col gap-5 ">
-        <div className="flex gap-2">
-          <TextField
-            required
-            id="fname"
-            label="First Name"
-            size="small"
-            className="max-w-[130px]"
-          />
-          <TextField
-            required
-            id="mname"
-            label="Middle Name"
-            size="small"
-            className="max-w-[130px]"
-          />
-          <TextField
-            required
-            id="lname"
-            label="Last Name"
-            size="small"
-            className="max-w-[130px]"
-          />
-        </div>
-        <div className="flex gap-2">
-          <FormControl sx={{ minWidth: 120 }} className="flex-1" size="small">
-            <InputLabel id="country">Country</InputLabel>
-            <Select
-              required
-              labelId="country"
-              id="country"
-              // value={age}
-              // onChange={handleChange}
-              label="Country"
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={10}>Cairo</MenuItem>
-              <MenuItem value={20}>Test 1</MenuItem>
-              <MenuItem value={30}>Test 2</MenuItem>
-            </Select>
-          </FormControl>
+    // STEP 1
 
-          <FormControl sx={{ minWidth: 120 }} className="flex-1" size="small">
-            <InputLabel id="city">City</InputLabel>
-            <Select
-              required
-              labelId="city"
-              id="city"
-              // value={age}
-              // onChange={handleChange}
-              label="City"
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={10}>Cairo</MenuItem>
-              <MenuItem value={20}>Test 1</MenuItem>
-              <MenuItem value={30}>Test 2</MenuItem>
-            </Select>
-          </FormControl>
-
-          <FormControl sx={{ minWidth: 120 }} className="flex-1" size="small">
-            <InputLabel id="district">District</InputLabel>
-            <Select
-              required
-              labelId="district"
-              id="district"
-              // value={age}
-              // onChange={handleChange}
-              label="district"
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={10}>Cairo</MenuItem>
-              <MenuItem value={20}>Test 1</MenuItem>
-              <MenuItem value={30}>Test 2</MenuItem>
-            </Select>
-          </FormControl>
-        </div>
-        <div className="w-full">
-          <TextField
-            required
-            id="phonenumber"
-            label="Phone Number"
-            size="small"
-            className="w-full"
-          />
-        </div>
-        <div>
-          <FormControl sx={{ minWidth: 220 }} className="w-full" size="small">
-            <InputLabel id="nationality">Nationality</InputLabel>
-            <Select
-              required
-              labelId="nationality"
-              id="nationality"
-              // value={age}
-              // onChange={handleChange}
-              label="nationality"
-            >
-              <MenuItem value="">
-                <em>None</em>
-              </MenuItem>
-              <MenuItem value={10}>Cairo</MenuItem>
-              <MenuItem value={20}>Test 1</MenuItem>
-              <MenuItem value={30}>Test 2</MenuItem>
-            </Select>
-          </FormControl>
-        </div>
-      </div>
-    </ThemeProvider>,
-
-    <>
-      <div className="mt-5 flex w-full max-w-[380px] flex-col items-center gap-3">
-        <input type="text" placeholder="Email" className={styles.input} />
-        <input type="text" placeholder="Password" className={styles.input} />
-        <input
-          type="text"
-          placeholder="Confirm Password"
-          className={styles.input}
+    <div className="flex flex-col gap-5 ">
+      <div className="flex gap-2">
+        <TextField
+          required
+          id="fname"
+          label="First Name"
+          size="small"
+          className="max-w-[130px]"
+        />
+        <TextField
+          required
+          id="mname"
+          label="Middle Name"
+          size="small"
+          className="max-w-[130px]"
+        />
+        <TextField
+          required
+          id="lname"
+          label="Last Name"
+          size="small"
+          className="max-w-[130px]"
         />
       </div>
-    </>,
-    <div className="flex flex-col">
-      <label htmlfor="cars">Choose a car</label>
+      <div className="flex gap-2">
+        <FormControl sx={{ minWidth: 120 }} className="flex-1" size="small">
+          <InputLabel id="country">Country</InputLabel>
+          <Select
+            required
+            labelId="country"
+            id="country"
+            // value={age}
+            // onChange={handleChange}
+            label="Country"
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Cairo</MenuItem>
+            <MenuItem value={20}>Test 1</MenuItem>
+            <MenuItem value={30}>Test 2</MenuItem>
+          </Select>
+        </FormControl>
 
-      <select
-        name="cars"
-        className="cursor-pointer rounded-lg bg-gray-200 px-3 py-1"
-      >
-        <option value="volvo">Volvo</option>
-        <option value="saab">Saab</option>
-        <option value="mercedes">Mercedes</option>
-        <option value="audi">Audi</option>
-      </select>
+        <FormControl sx={{ minWidth: 120 }} className="flex-1" size="small">
+          <InputLabel id="city">City</InputLabel>
+          <Select
+            required
+            labelId="city"
+            id="city"
+            // value={age}
+            // onChange={handleChange}
+            label="City"
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Cairo</MenuItem>
+            <MenuItem value={20}>Test 1</MenuItem>
+            <MenuItem value={30}>Test 2</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl sx={{ minWidth: 120 }} className="flex-1" size="small">
+          <InputLabel id="district">District</InputLabel>
+          <Select
+            required
+            labelId="district"
+            id="district"
+            // value={age}
+            // onChange={handleChange}
+            label="district"
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Cairo</MenuItem>
+            <MenuItem value={20}>Test 1</MenuItem>
+            <MenuItem value={30}>Test 2</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <div className="w-full">
+        <TextField
+          required
+          id="phonenumber"
+          label="Phone Number"
+          size="small"
+          className="w-full"
+        />
+      </div>
+      <div>
+        <FormControl sx={{ minWidth: 220 }} className="w-full" size="small">
+          <InputLabel id="nationality">Nationality</InputLabel>
+          <Select
+            required
+            labelId="nationality"
+            id="nationality"
+            // value={age}
+            // onChange={handleChange}
+            label="nationality"
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Cairo</MenuItem>
+            <MenuItem value={20}>Test 1</MenuItem>
+            <MenuItem value={30}>Test 2</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+    </div>,
+    // STEP 2
+
+    <div className="flex flex-col gap-y-5 pt-5">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DesktopDatePicker
+          label="Date of Birth"
+          inputFormat="MM/DD/YYYY"
+          // value={value}
+          // onChange={handleChange}
+          renderInput={(params) => <TextField {...params} />}
+        />
+      </LocalizationProvider>
+
+      <div className="flex justify-around gap-2">
+        <div>
+          <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="female"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel
+              value="female"
+              control={<Radio />}
+              label="Female"
+            />
+            <FormControlLabel value="male" control={<Radio />} label="Male" />
+          </RadioGroup>
+        </div>
+        <div>
+          <FormLabel id="demo-radio-buttons-group-label">
+            Student Type
+          </FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="female"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel
+              value="schoolstudent"
+              control={<Radio />}
+              label="School Student"
+            />
+            <FormControlLabel
+              value="universitystudent"
+              control={<Radio />}
+              label="University Student"
+            />
+          </RadioGroup>
+        </div>
+      </div>
+
+      <div className="flex gap-2 ">
+        <FormControl sx={{ minWidth: 220 }} className="flex-1" size="small">
+          <InputLabel id="Grade">Grade</InputLabel>
+          <Select
+            required
+            labelId="Grade"
+            id="Grade"
+            label="Grade"
+            // value={age}
+            // onChange={handleChange}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Test 0</MenuItem>
+            <MenuItem value={20}>Test 1</MenuItem>
+            <MenuItem value={30}>Test 2</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl sx={{ minWidth: 220 }} className="flex-1" size="small">
+          <InputLabel id="CertificateType">Certificate Type</InputLabel>
+          <Select
+            required
+            labelId="CertificateType"
+            id="CertificateType"
+            label="CertificateType"
+            // value={age}
+            // onChange={handleChange}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Test 0</MenuItem>
+            <MenuItem value={20}>Test 1</MenuItem>
+            <MenuItem value={30}>Test 2</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+
+      <div className="flex gap-2 ">
+        <FormControl sx={{ minWidth: 220 }} className="flex-1" size="small">
+          <InputLabel id="Major">Major</InputLabel>
+          <Select
+            required
+            labelId="Major"
+            id="Major"
+            label="Major"
+            // value={age}
+            // onChange={handleChange}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Test 0</MenuItem>
+            <MenuItem value={20}>Test 1</MenuItem>
+            <MenuItem value={30}>Test 2</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl sx={{ minWidth: 220 }} className="flex-1" size="small">
+          <InputLabel id="Degree">Degree</InputLabel>
+          <Select
+            required
+            labelId="Degree"
+            id="Degree"
+            label="Degree"
+            // value={age}
+            // onChange={handleChange}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={10}>Test 0</MenuItem>
+            <MenuItem value={20}>Test 1</MenuItem>
+            <MenuItem value={30}>Test 2</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+    </div>,
+
+    // STEP 3
+    <div className="flex w-[70%] flex-col gap-y-5">
+      <TextField
+        required
+        id="email"
+        label="Email"
+        size="small"
+        className="w-full"
+      />
+      <TextField
+        required
+        id="password"
+        label="Password"
+        size="small"
+        className="w-full"
+      />
+      <TextField
+        required
+        id="cpassword"
+        label="Confirm Password"
+        size="small"
+        className="w-full"
+      />
     </div>,
   ][activeStep];
 }
@@ -245,26 +382,30 @@ function Signup() {
       {/* Student Signup */}
       <div className=" flex flex-[3] flex-col bg-white pb-10 md:pb-0">
         <div className="flex flex-1 flex-col items-center justify-center">
-          <div className="flex min-h-[25rem] flex-col items-center justify-between">
+          <div className="flex min-h-[35rem] flex-col items-center justify-between">
             <h1 className={styles.h1 + " mt-10 md:mt-0"}>
-              Create Your Account {activeStep}
+              Create Your Account
             </h1>
 
-            <div>
+            <ThemeProvider theme={theme}>
               <SignupForm activeStep={activeStep} />
-            </div>
+            </ThemeProvider>
 
-            <div className="flex w-full flex-row justify-around">
+            <div className="mt-5 flex w-full flex-row items-center justify-around">
               <button
-                className="w-fit rounded-lg bg-[#950003] px-10 py-3 text-white hover:bg-[#bb0003] focus:outline-none disabled:opacity-50"
+                className="w-[100px] rounded-lg bg-[#950003]  py-3 text-white hover:bg-[#bb0003] focus:outline-none disabled:opacity-50"
                 onClick={handleStepBack}
                 disabled={activeStep === 0}
               >
                 Back
               </button>
 
+              <h5>
+                {activeStep + 1} / {STEP_COUNT}
+              </h5>
+
               <button
-                className="w-fit rounded-lg bg-[#950003] px-10 py-3 text-white hover:bg-[#bb0003] focus:outline-none disabled:opacity-50"
+                className="w-[100px] rounded-lg bg-[#950003] py-3 text-white hover:bg-[#bb0003] focus:outline-none disabled:opacity-50"
                 onClick={
                   activeStep !== STEP_COUNT - 1 ? handleStepNext : submitForm
                 }
