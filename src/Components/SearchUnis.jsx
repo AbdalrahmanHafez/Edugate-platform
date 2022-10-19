05d1b93623c4a5f38b8fc15d34693f1c75e45d94
Abdelrahman Hafez <dahomy999@outlook.sa>
2022-10-19 10:44:50 +0200

    remove dirty

--- src/Components/SearchUnis.jsx
+++ src/Components/SearchUnis.jsx
@@ -3,80 +3,81 @@ import { FaCaretDown, FaAesymmetrik } from "react-icons/fa";
 import { AiOutlineClose } from "react-icons/ai";
 import { RiCheckboxCircleLine } from "react-icons/ri";
 import MultiRangeSlider from "Components/multiRangeSlider/MultiRangeSlider";
+
 import { BsSearch } from "react-icons/bs";
 
 export default function SearchUnis({ inHomePage }) {
   const SearchBar = () => (
     <div
       className={
-        "w-full h-fit mb-2 py-2 px-5 bg-white rounded-[11px] shadow-md" +
+        "mb-2 h-fit w-full rounded-[11px] bg-white py-2 px-5 shadow-md" +
         (inHomePage ? " shadow-[0_5px_30px_#c5c5c54a]" : "")
       }
     >
       {/* Inner container */}
-      <div className="w-full h-full flex flex-col lg:flex-row space-x-1">
+      <div className="flex h-full w-full flex-col space-x-1 lg:flex-row">
         {/* Search input */}
-        <div className="flex-[1] h-full">
+        <div className="h-full flex-[1]">
           <input
             type="text"
             placeholder="Search University name"
-            className="w-full h-full px-4 py-2 bg-gray-200 text-black rounded-[11px] outline-none focus:ring-2 focus:ring-[#afafaf80] focus:ring-opacity-50"
+            className="h-full w-full rounded-[11px] bg-gray-200 px-4 py-2 text-black outline-none focus:ring-2 focus:ring-[#afafaf80] focus:ring-opacity-50"
           />
         </div>
 
         {/* Filter menus */}
-        <div className="flex-[1] flex lg:justify-end lg:mt-0 justify-center mt-2 items-center space-x-3">
+        <div className="mt-2 flex flex-[1] items-center justify-center space-x-3 lg:mt-0 lg:justify-end">
           {/* Faculty and Major */}
-          <div className="relative group">
+          <div className="group relative">
             <label className={styles.filterlabel}>
               Faculty & Major <FaCaretDown />
             </label>
 
             <div
-              className="absolute hidden group-hover:flex group-focus-within:flex lg:flex-row flex-col z-10 lg:w-96 w-40 h-fit bg-white p-2 lg:space-x-4 lg:space-y-0 space-y-2 rounded shadow-lg"
+              className="absolute z-10 hidden h-fit w-40 flex-col space-y-2 rounded bg-white p-2 shadow-lg group-focus-within:flex group-hover:flex lg:w-96 lg:flex-row lg:space-x-4 lg:space-y-0"
               style={{ inset: "100% 0 auto auto" }}
             >
-              <div className="flex-1 flex flex-col">
-                <div className="w-full mb-1">
+              <div className="flex flex-1 flex-col">
+                <div className="mb-1 w-full">
                   <label className="text-sm">Faculty</label>
                   <input
                     type="text"
                     placeholder="Search"
-                    className="w-full text-sm bg-slate-200 p-1 rounded outline-none focus:ring-2 focus:ring-[#8a8a8a] focus:ring-opacity-50"
+                    className="w-full rounded bg-slate-200 p-1 text-sm outline-none focus:ring-2 focus:ring-[#8a8a8a] focus:ring-opacity-50"
                   />
                 </div>
-                <div className="overflow-y-auto grow">
-                  <label className="flex items-center hover:bg-slate-200 rounded cursor-pointer px-2">
+                <div className="grow overflow-y-auto">
+                  <label className="flex cursor-pointer items-center rounded px-2 hover:bg-slate-200">
                     Faculty 1
                     <RiCheckboxCircleLine className="ml-auto" />
                   </label>
-                  <label className="flex items-center hover:bg-slate-200 rounded cursor-pointer px-2">
+                  <label className="flex cursor-pointer items-center rounded px-2 hover:bg-slate-200">
                     Faculty 2
                   </label>
-                  <label className="flex items-center hover:bg-slate-200 rounded cursor-pointer px-2">
+                  <label className="flex cursor-pointer items-center rounded px-2 hover:bg-slate-200">
                     Faculty 3
                   </label>
                 </div>
               </div>
 
-              <div className="flex-1 flex flex-col">
-                <div className="w-full mb-1">
+              <div className="flex flex-1 flex-col">
+                <div className="mb-1 w-full">
                   <label className="text-sm">Major</label>
                   <input
                     type="text"
                     placeholder="Search"
-                    className="w-full text-sm bg-slate-200 p-1 rounded outline-none focus:ring-2 focus:ring-[#8a8a8a] focus:ring-opacity-50"
+                    className="w-full rounded bg-slate-200 p-1 text-sm outline-none focus:ring-2 focus:ring-[#8a8a8a] focus:ring-opacity-50"
                   />
                 </div>
-                <div className="overflow-y-auto grow">
-                  <label className="flex items-center hover:bg-slate-200 rounded cursor-pointer px-2">
+                <div className="grow overflow-y-auto">
+                  <label className="flex cursor-pointer items-center rounded px-2 hover:bg-slate-200">
                     Major 1
                     <RiCheckboxCircleLine className="ml-auto" />
                   </label>
-                  <label className="flex items-center hover:bg-slate-200 rounded cursor-pointer px-2">
+                  <label className="flex cursor-pointer items-center rounded px-2 hover:bg-slate-200">
                     Major 2
                   </label>
-                  <label className="flex items-center hover:bg-slate-200 rounded cursor-pointer px-2">
+                  <label className="flex cursor-pointer items-center rounded px-2 hover:bg-slate-200">
                     Major 3
                   </label>
                 </div>
@@ -85,27 +86,27 @@ export default function SearchUnis({ inHomePage }) {
           </div>
 
           {/* City */}
-          <div className="relative group">
+          <div className="group relative">
             <label className={styles.filterlabel}>
               City <FaCaretDown />
             </label>
 
             {/* City Filter */}
             <div
-              className="hidden group-focus-within:flex group-hover:flex absolute z-10 w-48 h-64 flex-col p-2 rounded shadow-lg bg-white"
+              className="absolute z-10 hidden h-64 w-48 flex-col rounded bg-white p-2 shadow-lg group-focus-within:flex group-hover:flex"
               style={{ inset: "100% 0 auto auto" }}
             >
               <input
                 type="text"
                 placeholder="Search Cities"
-                className="w-full mb-2 bg-slate-200 p-1 rounded outline-none focus:ring-2 focus:ring-[#8a8a8a] focus:ring-opacity-50"
+                className="mb-2 w-full rounded bg-slate-200 p-1 outline-none focus:ring-2 focus:ring-[#8a8a8a] focus:ring-opacity-50"
               />
-              <div className="overflow-y-auto grow">
-                <label className="flex items-center hover:bg-slate-200 rounded cursor-pointer px-2">
+              <div className="grow overflow-y-auto">
+                <label className="flex cursor-pointer items-center rounded px-2 hover:bg-slate-200">
                   Item 1
                   <RiCheckboxCircleLine className="ml-auto" />
                 </label>
-                <label className="flex items-center hover:bg-slate-200 rounded cursor-pointer px-2">
+                <label className="flex cursor-pointer items-center rounded px-2 hover:bg-slate-200">
                   Item 2
                 </label>
               </div>
@@ -113,18 +114,18 @@ export default function SearchUnis({ inHomePage }) {
           </div>
 
           {/* Budget */}
-          <div className="relative group">
+          <div className="group relative">
             <label className={styles.filterlabel}>
               Budget <FaCaretDown />
             </label>
 
             <div
-              className="absolute hidden group-hover:flex flex-col z-10 w-48 h-64 bg-white p-2 rounded shadow-lg"
+              className="absolute z-10 hidden h-64 w-48 flex-col rounded bg-white p-2 shadow-lg group-hover:flex"
               style={{ inset: "100% 0 auto auto" }}
             >
               {/* price slider */}
               <label className="text-sm">Range</label>
-              <div className="w-full h-10 py-3 px-2 text-black mb-5">
+              <div className="mb-5 h-10 w-full py-3 px-2 text-black">
                 <MultiRangeSlider
                   min={0}
                   max={1000}
@@ -135,9 +136,9 @@ export default function SearchUnis({ inHomePage }) {
               </div>
 
               {/* OR divider */}
-              <div className="relative flex py-1 items-center mb-2">
+              <div className="relative mb-2 flex items-center py-1">
                 <div className="flex-grow border-t border-gray-400"></div>
-                <span className="flex-shrink text-xs mx-4 text-gray-400">
+                <span className="mx-4 flex-shrink text-xs text-gray-400">
                   OR
                 </span>
                 <div className="flex-grow border-t border-gray-400"></div>
@@ -145,15 +146,15 @@ export default function SearchUnis({ inHomePage }) {
 
               {/* price range presets*/}
               <label className="text-sm">Preset</label>
-              <div className="overflow-y-auto grow">
-                <label className="flex items-center hover:bg-slate-200 rounded cursor-pointer px-2">
+              <div className="grow overflow-y-auto">
+                <label className="flex cursor-pointer items-center rounded px-2 hover:bg-slate-200">
                   Price range 1
                   <RiCheckboxCircleLine className="ml-auto" />
                 </label>
-                <label className="flex items-center hover:bg-slate-200 rounded cursor-pointer px-2">
+                <label className="flex cursor-pointer items-center rounded px-2 hover:bg-slate-200">
                   Price range 2
                 </label>
-                <label className="flex items-center hover:bg-slate-200 rounded cursor-pointer px-2">
+                <label className="flex cursor-pointer items-center rounded px-2 hover:bg-slate-200">
                   Price range 3
                 </label>
               </div>
@@ -161,7 +162,7 @@ export default function SearchUnis({ inHomePage }) {
           </div>
 
           {inHomePage && (
-            <button className="text-black p-2 rounded-full text-xl bg-white focus:ring-2 ring-gray-500">
+            <button className="rounded-full bg-white p-2 text-xl text-black ring-gray-500 focus:ring-2">
               <BsSearch />
             </button>
           )}
@@ -173,12 +174,12 @@ export default function SearchUnis({ inHomePage }) {
   if (inHomePage) return <SearchBar />;
 
   return (
-    <div className="w-full h-full ">
+    <div className="h-full w-full ">
       {/* Search bar */}
       <SearchBar />
 
       {/* Filtered Elements */}
-      <div className="overflow-x-auto overflow-y-hidden w-full h-12 mb-3 min-h-[48px] flex flex-nowrap items-center gap-2 px-5 rounded-[11px] shadow-md bg-[#FDFDFD]">
+      <div className="mb-3 flex h-12 min-h-[48px] w-full flex-nowrap items-center gap-2 overflow-x-auto overflow-y-hidden rounded-[11px] bg-[#FDFDFD] px-5 shadow-md">
         <label className={styles.filteritemlabel}>
           City Cairo
           <AiOutlineClose />
@@ -210,23 +211,23 @@ export default function SearchUnis({ inHomePage }) {
           <AiOutlineClose />
         </label>
 
-        <label className={styles.filteritemlabel + " bg-red-800 ml-auto"}>
+        <label className={styles.filteritemlabel + " ml-auto bg-red-800"}>
           Clear All
         </label>
       </div>
 
       {/* Search Results */}
-      <div className="w-full h-full p-5 bg-[#FDFDFD] rounded-[11px] flex justify-center flex-wrap shadow-md">
+      <div className="flex h-full w-full flex-wrap justify-center rounded-[11px] bg-[#FDFDFD] p-5 shadow-md">
         {Array(20)
           .fill()
           .map((_, i) => (
             <div className={styles.searchresultcard}>
               <img
-                className="object-contain w-full rounded-lg"
+                className="w-full rounded-lg object-contain"
                 src={`/uni_logos/${(i % 14) + 1}.png`}
                 alt=""
               />
-              <div className="leading-4 text-sm">
+              <div className="text-sm leading-4">
                 German University in Cairo {i}
               </div>
             </div>
